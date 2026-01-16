efa926d12e93a6b30b1c1fa4b23376fe662e3ce9
Liam Tat Tze Tey <tattzetey@gmail.com>
2026-01-16 23:12:07 +0000

    add use cases

---
+++ app/service/src/use-case/integration-basic-credential/update-integration-basic-credential.use-case.ts
@@ -0,0 +1,58 @@
+import { UTCDate } from "@date-fns/utc";
+import { and, eq } from "drizzle-orm";
+import { isNil } from "es-toolkit";
+import { ResultAsync, err } from "neverthrow";
+import type z from "zod";
+import type { currentUserSchema } from "@/guard/auth-check.guard";
+import type { Ctx } from "@/lib/ctx";
+import { Err } from "@/lib/err";
+import { integrationBasicCredentialTable } from "@/schema/integration-basic-credential.schema";
+
+type Payload = {
+  id: string;
+  body: {
+    username?: string;
+    password?: string;
+    expiredAt?: Date;
+  };
+  user: z.infer<typeof currentUserSchema>;
+};
+
+export const updateIntegrationBasicCredentialUseCase = async (
+  ctx: Ctx,
+  { id, body, user }: Payload,
+) => {
+  const existsResult = await ResultAsync.fromPromise(
+    ctx.db.query.integrationBasicCredential.findFirst({
+      where: { id, organizationId: user.organizationId },
+    }),
+    (e) => Err.from(e),
+  );
+
+  if (existsResult.isErr()) {
+    return existsResult;
+  }
+
+  if (isNil(existsResult.value)) {
+    return err(Err.code("notFound"));
+  }
+
+  return ResultAsync.fromPromise(
+    ctx.db
+      .update(integrationBasicCredentialTable)
+      .set({
+        ...body,
+        updatedAt: new UTCDate(),
+      })
+      .where(
+        and(
+          eq(integrationBasicCredentialTable.id, id),
+          eq(
+            integrationBasicCredentialTable.organizationId,
+            user.organizationId,
+          ),
+        ),
+      ),
+    (e) => Err.from(e),
+  );
+};
