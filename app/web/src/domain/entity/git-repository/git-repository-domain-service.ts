08908cbeef2f71a9acb650189c20af453ed288a1
Liam Tat Tze Tey <tattzetey@gmail.com>
2026-02-21 18:02:09 +0800

    rebase

--- app/web/src/domain/entity/git-repository/git-repository-domain-service.ts
+++ app/web/src/domain/entity/git-repository/git-repository-domain-service.ts
@@ -8,12 +8,10 @@ type MakeGitRepositoryDomainServiceInput = {
 export const makeGitRepositoryDomainService = ({
   gitRepository,
 }: MakeGitRepositoryDomainServiceInput) => ({
-  getCreatedAt: () =>
-    format(gitRepository.createdAt, "MMM d, yyyy, h:mm a"),
-  getUpdatedAt: () =>
-    format(gitRepository.updatedAt, "MMM d, yyyy, h:mm a"),
   getProviderDisplayName: () =>
     providerToDisplayName[gitRepository.integration.provider],
+  getCreatedAt: () => format(gitRepository.createdAt, "MMM d, yyyy, h:mm a"),
+  getUpdatedAt: () => format(gitRepository.updatedAt, "MMM d, yyyy, h:mm a"),
 });
 
 const providerToDisplayName = {
