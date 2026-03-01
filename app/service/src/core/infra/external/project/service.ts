455f28b664b25d0164deae7a8cc1445855433382
Liam Tat Tze Tey <tattzetey@gmail.com>
2026-03-01 15:26:15 +0800

    add credential and project entities as input

--- app/service/src/core/infra/external/project/service.ts
+++ app/service/src/core/infra/external/project/service.ts
@@ -2,20 +2,28 @@ import type { Result } from "neverthrow";
 import z from "zod";
 import { tenantCtxSchema } from "@/common/ctx/tenant-ctx";
 import { AiAgentEntity } from "@/core/domain/ai-agent/entity";
+import { CredentialEntity } from "@/core/domain/credential/entity";
 import { GitRepositoryEntity } from "@/core/domain/git-repository/entity";
+import { ProjectEntity } from "@/core/domain/project/entity";
 import { WorkflowDefinitionEntity } from "@/core/domain/workflow/definition/entity";
 
 const externalProjectServiceInputSchema = {
   syncAiAgentIssueField: z.object({
     ctx: tenantCtxSchema,
+    credential: z.instanceof(CredentialEntity),
+    project: z.instanceof(ProjectEntity),
     aiAgent: z.instanceof(AiAgentEntity),
   }),
   syncGitRepositoryIssueField: z.object({
     ctx: tenantCtxSchema,
+    credential: z.instanceof(CredentialEntity),
+    project: z.instanceof(ProjectEntity),
     gitRepository: z.instanceof(GitRepositoryEntity),
   }),
   syncWorkflowDefinitionIssueField: z.object({
     ctx: tenantCtxSchema,
+    credential: z.instanceof(CredentialEntity),
+    project: z.instanceof(ProjectEntity),
     workflowDefinition: z.instanceof(WorkflowDefinitionEntity),
   }),
 };
